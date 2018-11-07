0f4624d5714bdfb6fa04897534e32471118146d1
Demailly <pierredemailly.pro@gmail.com>
2018-11-07 10:03:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# HTML5 Boilerplate
+## Instructions
+* Download or Clone the repo
+* Start coding
+## Included
+* Modernizr
+* Normalize
+* jQuery
+* Bootstrap
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Localhost</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <link rel="apple-touch-icon" href="./favicon.png">
+  <link rel="shortcut icon" type="image/png" href="./favicon.png" />
+
+  <link rel="stylesheet" href="./css/main.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <!-- <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet"> -->
+</head>
+
+<body>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <div class="container">
+    <h1>Html5 Boilerplate</h1>
+    <div class="alert alert-success">
+      <p class="h2">Included</p>
+      <ul>
+        <li>Normalize <small><a href="https://necolas.github.io/normalize.css/" alt="Link to Normalize" title="Link to Normalize">https://necolas.github.io/normalize.css/</a></small></li>
+        <li>Modernizr <small><a href="https://modernizr.com/" alt="Link to Modernizr" title="Link to Modernizr">https://modernizr.com/</a></small></li>
+        <li>jQuery <small><a href="https://jquery.com/" alt="Link to jQuery" alt="Link to jQuery">https://jquery.com/</a></small></li>
+        <li>Bootstrap <small>(CDN) <a href="https://getbootstrap.com/" alt="Link to Bootstrap" title="Link to Bootstrap">https://getbootstrap.com/</a></small></li>
+      </ul>
+    </div>
+    <p class="text-right font-italic">Have fun coding</p>
+  </div>
+
+  <script src="./js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="./js/vendor/jquery-3.3.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+  <script src="./js/main.js"></script>
+
+  <!-- Global Site Tag (gtag.js) - Google Analytics
+  <script async src="https://www.googletagmanager.com/gtag/js?id=GA_TRACKING_ID"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'GA_TRACKING_ID');
+  </script> -->
+</body>
+
+</html>
